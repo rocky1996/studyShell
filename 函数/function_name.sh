d2c4c4b6eea9bc2ac1f2c02915fd7f74744b2c6a
rocky1996 <2791752775@qq.com>
2019-01-17 14:19:11 +0800

    添加函数function的shell编程

---
+++ 函数/function_name.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#by authors wujinfan time 2019/01/17
+
+function Apache_install(){
+	echo -e "\033[32mThe Apache Server wait install...\033[0m"
+}
+
+function Mysql_install(){
+	echo -e "\033[32mThe Mysql Server wait install...\033[0m"
+}
+
+function PHP_install(){
+	echo -e "\033[32mThe PHP Server wait install...\033[0m"
+}
+
+Apache_install
+Mysql_install
+PHP_install
+
